64e0c8f4d9c495ef10d10da77d3f5b484ad410af
Jeremy Leonard <jeremyl@gracon.com>
2022-03-17 14:27:17 -0400

    Finished Sophos XG API.

--- classes/SophosXGAPI.class.php
+++ classes/SophosXGAPI.class.php
@@ -13,44 +13,92 @@
  */
 class SophosXGAPI {
     
+    const IPFamily_IPv4='IPv4';
+    const IPFamily_IPv6='IPv6';
+    const HostType_IP='IP';
+    const HostType_IPRange='IPRange';
+    const HostType_IPList='IPList';
+    const HostType_Network='Network';
+
     public function __construct(Config $config) {
         $this->config = $config;
     }
-    public function getFQDNHostGroup($name){
-        $xml=sprintf('<Get><FQDNHostGroup><Name>%s</Name></FQDNHostGroup></Get>',$name);
+ 
+    public function getIPHost($name){
+        $xml=sprintf('<Get><IPHost><Name>%s</Name></IPHost></Get>',$name);
         $domxpath = $this->doAPI($xml);
 
-        foreach($domxpath->evaluate('//FQDNHostGroup') AS $FQDNHostGroup){
-            if($FQDNHostGroup->getElementsByTagName('Name')[0]->nodeValue==$name){
+        foreach($domxpath->evaluate('//IPHost') AS $IPHost){
+            $attrs = [
+                'Name',
+                'IPFamily',
+                'HostType',
+                'IPAddress',
+                'Subnet',
+                'StartIPAddress',
+                'EndIPAddress',
+                'ListOfIPAddresses'
+            ];
+            if($IPHost->getElementsByTagName('Name')[0]->nodeValue==$name){
                 $obj = new stdClass();
-                $obj->Name = $FQDNHostGroup->getElementsByTagName('Name')[0]->nodeValue;
-                $obj->Description = $FQDNHostGroup->getElementsByTagName('Description')[0]->nodeValue;
-                foreach($FQDNHostGroup->getElementsByTagName('FQDNHostList')[0]->getElementsByTagName('FQDNHost') AS $FQDNHosts){
-                    $obj->FQDNHostList[]=$FQDNHosts->nodeValue;
+                foreach($attrs as $attr){
+                    echo $attr."\n";
+                    $obj->$attr = $IPHost->getElementsByTagName($attr)[0]->nodeValue ?? null;
+                }
+                if(count($IPHost->getElementsByTagName('HostGroupList')) > 0){
+                    foreach($IPHost->getElementsByTagName('HostGroupList')[0]->getElementsByTagName('HostGroup') AS $HostGroup){
+                        $obj->HostGroupList[]=$HostGroup->nodeValue;
+                    }
                 }
                 return $obj;
             }
         }
-    }    
-    public function addFQDNHostGroup($fqdn, $name=null, $FQDNHostGroupList=[]){
-        $name = $name ?? $fqdn;
-        if(count($FQDNHostGroupList) > 0){
-            $groups = "<FQDNHostGroupList>";
-            foreach($FQDNHostGroupList as $group){
-                $groups .= sprintf('<FQDNHostGroup>%s</FQDNHostGroup>',$group);
+        
+    }
+    public function addIPHost($name, $HostType, $HostGroup=[],$IPAddress, $Subnet=null, $StartIPAddress=null, $EndIPAddress=null, $ListOfIPAddresses=[], $IPFamily=SophosXGAPI::IPFamily_IPv4) {
+        if(count($HostGroup) > 0){
+            $groups = '<HostGroupList>';
+            foreach($HostGroup as $group){
+                $groups .= sprintf('<HostGroup>%s</HostGroup>',$group);
             }
-            $groups .= "</FQDNHostGroupList>";
+            $groups .= '</HostGroupList>';
         }else{
             $groups = '';
         }
-        $xml=sprintf('<Set><FQDNHost><Name>%s</Name><FQDN>%s</FQDN>%s</FQDNHost></Set>',$name,$fqdn,$groups);
+        switch ($HostType) {
+            case SophosXGAPI::HostType_IP:
+                $xml = sprintf('<Set><IPHost><Name>%s</Name><IPFamily>%s</IPFamily><HostType>IP</HostType><IPAddress>%s</IPAddress>%s</IPHost></Set>',$name,$IPFamily,$IPAddress,$groups);
+                break;
+            case SophosXGAPI::HostType_IPList:
+                $xml = sprintf('<Set><IPHost><Name>%s</Name><IPFamily>%s</IPFamily><HostType>IPList</HostType><ListOfIPAddresses>%s</ListOfIPAddresses>%s</IPHost></Set>',$name,$IPFamily,$ListOfIPAddresses,$groups);
+                break;
+            case SophosXGAPI::HostType_IPRange:
+                $xml = sprintf('<Set><IPHost><Name>%s</Name><IPFamily>%s</IPFamily><HostType>IPRange</HostType><StartIPAddress>%s</StartIPAddress><EndIPAddress>%s</EndIPAddress>%s</IPHost></Set>',$name,$IPFamily,$StartIPAddress,$EndIPAddress,$groups);
+                break;
+            case SophosXGAPI::HostType_Network:
+                $xml = sprintf('<Set><IPHost><Name>%s</Name><IPFamily>%s</IPFamily><HostType>Network</HostType><IPAddress>%s</IPAddress><Subnet>%s</Subnet>%s</IPHost></Set>',$name,$IPFamily,$IPAddress,$Subnet,$groups);
+                break;
+        }
+        error_log($xml);
+        return $this->doAPI($xml);
+    }
+    public function removeIPHost($name){
+        $IPHost = $this->getIPHost($name);
+        $this->addIPHost(
+                $name,
+                $IPHost->HostType,
+                [],
+                $IPHost->IPAddress,
+                $IPHost->Subnet,
+                $IPHost->StartIPAddress,
+                $IPHost->EndIPAddress,
+                $IPHost->ListOfIPAddresses,
+                $IPHost->IPFamily
+                );
+        $xml=sprintf('<Remove><IPHost><Name>%s</Name></IPHost></Remove>',$name);
         $this->doAPI($xml);
     }
-    public function removeFQDNHostGroup($name) {
-        $xml=sprintf('<Remove><FQDNHost><Name>%s</Name></FQDNHost></Remove>',$name);
-        $this->doAPI($xml);
-    }
-    
+        
     public function getFQDNHost($name){
         $xml=sprintf('<Get><FQDNHost><Name>%s</Name></FQDNHost></Get>',$name);
         $domxpath = $this->doAPI($xml);
@@ -60,8 +108,8 @@ class SophosXGAPI {
                 $obj = new stdClass();
                 $obj->Name = $FQDNHost->getElementsByTagName('Name')[0]->nodeValue;
                 $obj->FQDN = $FQDNHost->getElementsByTagName('FQDN')[0]->nodeValue;
-                foreach($FQDNHostGroup->getElementsByTagName('FQDNList')[0]->getElementsByTagName('FQDNHost') AS $FQDNHosts){
-                    $obj->FQDNGroupList[]=$FQDNGroups->nodeValue;
+                foreach($FQDNHost->getElementsByTagName('FQDNHostGroupList')[0]->getElementsByTagName('FQDNHostGroup') AS $FQDNHostGroup){
+                    $obj->FQDNHostGroupList[]=$FQDNHostGroup->nodeValue;
                 }
 
                 return $obj;
@@ -83,7 +131,80 @@ class SophosXGAPI {
         $this->doAPI($xml);
     }
     public function removeFQDNHost($name) {
+        $FQDNHost = $this->getFQDNHost($name);
+        $this->addFQDNHost($FQDNHost->FQDN, $FQDNHost->Name, []);
         $xml=sprintf('<Remove><FQDNHost><Name>%s</Name></FQDNHost></Remove>',$name);
+        $this->doAPI($xml);
+    }
+    
+    public function getIPHostGroup($name){
+        $xml=sprintf('<Get><IPHostGroup><Name>%s</Name></IPHostGroup></Get>',$name);
+        $domxpath = $this->doAPI($xml);
+
+        foreach($domxpath->evaluate('//IPHostGroup') AS $IPHostGroup){
+            if($IPHostGroup->getElementsByTagName('Name')[0]->nodeValue==$name){
+                $obj = new stdClass();
+                $obj->Name = $IPHostGroup->getElementsByTagName('Name')[0]->nodeValue;
+                $obj->IPFamily = $IPHostGroup->getElementsByTagName('IPFamily')[0]->nodeValue;
+                $obj->Description = $IPHostGroup->getElementsByTagName('Description')[0]->nodeValue;
+                if(count($IPHostGroup->getElementsByTagName('HostList'))){
+                    foreach($IPHostGroup->getElementsByTagName('HostList')[0]->getElementsByTagName('Host') AS $Host){
+                        $obj->HostList[]=$Host->nodeValue;
+                    }
+                }
+                return $obj;
+            }
+        }
+    }    
+    public function addIPHostGroup($name, $description=null, $HostList=[],$IPFamily= SophosXGAPI::IPFamily_IPv4){
+        if(count($HostList) > 0){
+            $hosts = "<HostList>";
+            foreach($HostList as $host){
+                $hosts .= sprintf('<Host>%s</Host>',$host);
+            }
+            $hosts .= "</HostList>";
+        }else{
+            $hosts = '';
+        }
+        $xml=sprintf('<Set><IPHostGroup><Name>%s</Name><IPFamily>%s</IPFamily><Description>%s</Description>%s</IPHostGroup></Set>',$name,$IPFamily,$description,$hosts);
+        $this->doAPI($xml);
+    }
+    public function removeIPHostGroup($name) {
+        $xml=sprintf('<Remove><IPHostGroup><Name>%s</Name></IPHostGroup></Remove>',$name);
+        $this->doAPI($xml);
+    }
+
+    public function getFQDNHostGroup($name){
+        $xml=sprintf('<Get><FQDNHostGroup><Name>%s</Name></FQDNHostGroup></Get>',$name);
+        $domxpath = $this->doAPI($xml);
+
+        foreach($domxpath->evaluate('//FQDNHostGroup') AS $FQDNHostGroup){
+            if($FQDNHostGroup->getElementsByTagName('Name')[0]->nodeValue==$name){
+                $obj = new stdClass();
+                $obj->Name = $FQDNHostGroup->getElementsByTagName('Name')[0]->nodeValue;
+                $obj->Description = $FQDNHostGroup->getElementsByTagName('Description')[0]->nodeValue;
+                foreach($FQDNHostGroup->getElementsByTagName('FQDNHostList')[0]->getElementsByTagName('FQDNHost') AS $FQDNHosts){
+                    $obj->FQDNHostList[]=$FQDNHosts->nodeValue;
+                }
+                return $obj;
+            }
+        }
+    }    
+    public function addFQDNHostGroup($name, $description=null, $FQDNHostList=[]){
+        if(count($FQDNHostList) > 0){
+            $hosts = "<FQDNHostList>";
+            foreach($FQDNHostList as $host){
+                $hosts .= sprintf('<FQDNHost>%s</FQDNHost>',$host);
+            }
+            $hosts .= "</FQDNHostList>";
+        }else{
+            $hosts = '';
+        }
+        $xml=sprintf('<Set><FQDNHostGroup><Name>%s</Name><Description>%s</Description>%s</FQDNHostGroup></Set>',$name,$description,$hosts);
+        $this->doAPI($xml);
+    }
+    public function removeFQDNHostGroup($name) {
+        $xml=sprintf('<Remove><FQDNHostGroup><Name>%s</Name></FQDNHostGroup></Remove>',$name);
         $this->doAPI($xml);
     }
     
