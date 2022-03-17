567188cc2c109fd73f0a22193dab97ee4f401487
Jeremy Leonard <jeremyl@gracon.com>
2022-03-17 14:27:31 -0400

    Contunued Testing.

--- main.php
+++ main.php
@@ -9,20 +9,20 @@
 include(__DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'autoloader.php');
 include(__DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php');
 
-$config = new Config();
+$config = new Config($argv[1] ?? null);
 
-$msft = new MSFTEndpoints($config);
-$endpoints = $msft->checkForUpdates();
-$urls=[];
-$ips=[];
-foreach($endpoints as $endpoint){
-    foreach($endpoint->urls as $url){
-        $urls[]=$url;
-    }
-    foreach($endpoint->ips as $ip){
-        $ips[]=$ip;
-    }
-}
+//$msft = new MSFTEndpoints($config);
+//$endpoints = $msft->checkForUpdates();
+//$urls=[];
+//$ips=[];
+//foreach($endpoints as $endpoint){
+//    foreach($endpoint->urls as $url){
+//        $urls[]=$url;
+//    }
+//    foreach($endpoint->ips as $ip){
+//        $ips[]=$ip;
+//    }
+//}
 //print_r(array_unique($urls));
 //print_r(array_unique($ips));
 
@@ -30,13 +30,30 @@ foreach($endpoints as $endpoint){
 
 $xg = new SophosXGAPI($config);
 
-$xg->addFQDNHost('*.1test.org','*.1test.net',['Microsoft Services']);
+//$xg->addFQDNHost('*.1test.org','*.1test.net',['Microsoft Services']);
 
-echo json_encode($xg->getFQDNHostGroup('Microsoft Services'),JSON_PRETTY_PRINT);
+//echo json_encode($xg->getFQDNHostGroup('Microsoft Services'),JSON_PRETTY_PRINT);
+//echo json_encode($xg->getFQDNHost('*.1test.net'),JSON_PRETTY_PRINT);
 
-//print_r($xg->getFQDNHost('*.1test.net'));
+//$xg->addFQDNHostGroup('1-Test','Testing',[]);
+//print_r($xg->addFQDNHost('*.1test.net','*.1test.net',['1-Test']));
+//$xg->removeFQDNHost('*.1test.net');
+//$xg->removeFQDNHostGroup('1-Test');
+
+
+//$xg->removeFQDNHostGroup('1-Test');
 
 //$xg->removeFQDNHost('*.1test.net');
+
+//print_r($xg->addIPHostGroup('Test Networks', 'Testing'));
+
+//print_r($xg->addIPHost('1TestHost', SophosXGAPI::HostType_IP, ['Test Networks'], '10.1.1.1'));
+//print_r($xg->addIPHost('1TestHost', SophosXGAPI::HostType_Network, [], '10.1.1.0','255.255.255.0'));
+print_r($xg->removeIPHost('1TestHost'));
+
+//print_r($xg->getIPHostGroup('Test Networks'));
+
+//$xg->removeIPHostGroup('Test Networks');
 
 //$config->write('localVersion',$msft->getCurrentVersion());
 $config->save();
